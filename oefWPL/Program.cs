4de1e8c22049b76ef6c864fd925f64bde758589a
BramPetersPXL <12401770@student.pxl.be>
2025-11-06 13:37:01 +0100

    DEMO-04 Foutmelding

--- oefWPL/Program.cs
+++ oefWPL/Program.cs
@@ -6,17 +6,26 @@
         {
             string user = "Uzui";
             string password = "Daddy";
+            int tries = 0;
 
-            Console.Write("Naam: ");
-            string userInput = Console.ReadLine();
-            Console.Write("Wachtwoord: ");
-            string passwordInput = Console.ReadLine();
-
-            if (userInput == user && passwordInput == password)
+            while (tries < 3)
             {
-                Console.WriteLine($"Welkom, {userInput}!");
-                return;
+                Console.Write("Naam: ");
+                string userInput = Console.ReadLine();
+                Console.Write("Wachtwoord: ");
+                string passwordInput = Console.ReadLine();
+
+                if (userInput == user && passwordInput == password)
+                {
+                    Console.WriteLine($"Welkom, {userInput}!");
+                    return;
+                }
+
+                tries++;
+                Console.WriteLine($"Foutieve login. {3 - tries} pogingen over.");
             }
+
+            Console.WriteLine("Alle pogingen gebruikt, toegang geweigerd");
         }
     }
 }
